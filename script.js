e3ff10b41c5ddb89ed1ada82908e382df78435e7
Chxrxnn <deep963496@gmail.com>
2024-07-08 12:17:42 +0530

    project added from local

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const btn=document.getElementById("btn");
+    const input = document.getElementById("input");
+    const qrImage = document.getElementById("qrImage");
+    const qrBox = document.getElementById("qrBox");
+
+    btn.addEventListener("click",function(){
+        if(input.value.length>0){
+            qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+input.value;
+        input.value="";
+        qrBox.classList.add("show");
+
+        }
+            else{
+                input.classList.add("error");
+                setTimeout(()=>{
+                    input.classList.remove("error");
+                    input.style.outline="none";
+                },1000);
+
+                input.style.outline="1px solid red";
+            }
+            });
+
+});
